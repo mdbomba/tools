d462bbb65c461ce4c74fa4f4746d3e7d73659a75
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2024-02-29 08:03:28 -0700

    v 20240229
    Adjusted to require script be ran using sudo

--- install-server.sh
+++ install-server.sh
@@ -1,4 +1,4 @@
-version='20240227'
+version='20240229'
 cd ~
 . ~/.bashrc
 #
@@ -12,8 +12,8 @@ echo ''
 #########################################
 # CHECK IF SCRIPT WAS STARTED USING SUDO
 #########################################
-if test "x$EUID" = "x"; then
-  echo ''; echo 'Please run this script using normal user rights (not sudo)'
+if ! test "x$EUID" = "x"; then
+  echo ''; echo 'Please run this script using sudo'
   echo ''
   exit
 fi
@@ -25,15 +25,15 @@ read -p  "Enter password for Chef Admin Account ($CHEF_ADMIN_ID): " $CHEF_ADMIN_
 # DOWNLOAD AND INSTALL CHEF SERVER (AUTOMATE + INFRA_SERVER + BUILDER)
 #######################################################################
 curl https://packages.chef.io/files/current/latest/chef-automate-cli/chef-automate_linux_amd64.zip | gunzip - > chef-automate && chmod +x chef-automate
-sudo ./chef-automate deploy --product automate --product infra-server
-sudo chef-server-ctl reconfigure
+./chef-automate deploy --product automate --product infra-server
+chef-server-ctl reconfigure
 
 #########################
 # CREATE CHEF ADMIN USER
 #########################
 if [ ! -f "./$CHEF_ADMIN_ID.pem" ]
   then
-    sudo chef-server-ctl user-create "$CHEF_ADMIN_ID" "$CHEF_ADMIN_FIRST" "$CHEF_ADMIN_LAST" "$CHEF_ADMIN_EMAIL" "$CHEF_ADMIN_PASSWORD" --filename "$CHEF_ADMIN_ID.pem"
+    chef-server-ctl user-create "$CHEF_ADMIN_ID" "$CHEF_ADMIN_FIRST" "$CHEF_ADMIN_LAST" "$CHEF_ADMIN_EMAIL" "$CHEF_ADMIN_PASSWORD" --filename "$CHEF_ADMIN_ID.pem"
 fi
 
 ###########################
@@ -41,7 +41,7 @@ fi
 ###########################
 if [ ! -f "./$CHEF_ORG-validator.pem" ]
   then
-    sudo chef-server-ctl org-create "$CHEF_ORG" "$CHEF_ORG_LONG" --association_user "$CHEF_ADMIN_ID" --filename "$CHEF_ORG-validator.pem"
+    chef-server-ctl org-create "$CHEF_ORG" "$CHEF_ORG_LONG" --association_user "$CHEF_ADMIN_ID" --filename "$CHEF_ORG-validator.pem"
 fi
 
 ###########################
