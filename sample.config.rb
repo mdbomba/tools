73ddae25d76507d82111e8cd84a78add0d2ce443
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2024-03-03 13:52:02 -0700

    Delete sample.config.rb

--- sample.config.rb
+++
@@ -1,7 +0,0 @@
-current_dir = File.dirname(__FILE__)
-log_level        :info
-log_location     STDOUT
-node_name        'mike'
-client_key       '/home/mike/.chef/mike.pem'
-chef_server_url  'https://chef-server/organizations/chef-demo'
-cookbook_path    'home/mike/chef-demo/cookbooks'
