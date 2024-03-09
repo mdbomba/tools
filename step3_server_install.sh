9bc26519f8168f9152bdbc363681a0537300a9d6
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2024-03-08 22:48:14 -0700

    v20240308

--- step3_server_install.sh
+++ step3_server_install.sh
@@ -1,30 +1,27 @@
-version='20240229'
-cd ~
-. ~/.bashrc
-#
+version='20240308'
 # This script installs chef server with automate on either Ubunto or Redhat Linux
-#
-echo 'This script installs chef server (automate + infra-server)'
-echo 'This script should be started using user level (not sudo) rights.'
+echo 'Step3 script installs chef automate + infra server.'
 echo "Version = $version"
 echo ''
-
-#########################################
-# CHECK IF SCRIPT WAS STARTED USING SUDO
-#########################################
+###################################################
+# ENSURE SCRIPT WAS STARTED NOT STARTED USING SUDO
+###################################################
 if test "x$EUID" = "x"; then
-  echo ''; echo 'Please do not run this script using sudo'
+  echo ''; echo 'This script should be started using user level (not sudo) rights. Script is terminating.'
   echo ''
   exit
 fi
-
+#
+cd ~
+. ./.bashrc
+#
 #######################################################################
 # DOWNLOAD AND INSTALL CHEF SERVER (AUTOMATE + INFRA_SERVER + BUILDER)
 #######################################################################
 curl https://packages.chef.io/files/current/latest/chef-automate-cli/chef-automate_linux_amd64.zip | gunzip - > chef-automate && chmod +x chef-automate
 sudo ./chef-automate deploy --product automate --product infra-server
 sudo chef-automate init-config
-
+#
 #########################
 # CREATE CHEF ADMIN USER
 #########################
@@ -33,7 +30,7 @@ if [ ! -f "./$CHEF_ADMIN_ID.pem" ]
   then
     sudo chef-server-ctl user-create "$CHEF_ADMIN_ID" "$CHEF_ADMIN_FIRST" "$CHEF_ADMIN_LAST" "$CHEF_ADMIN_EMAIL" "$CHEF_ADMIN_PASSWORD" --filename "$CHEF_ADMIN_ID.pem"
 fi
-
+#
 ###########################
 # CREATE CHEF ORGANIZATION
 ###########################
@@ -41,7 +38,7 @@ if [ ! -f "./$CHEF_ORG""-validator.pem" ]
   then
     sudo chef-server-ctl org-create "$CHEF_ORG" "$CHEF_ORG_LONG" --association_user "$CHEF_ADMIN_ID" --filename "$CHEF_ORG-validator.pem"
 fi
-
+#
 ###########################
 # CREATE SSL KEY
 ###########################
@@ -50,4 +47,3 @@ cd ~/.ssh
 rm -f "$hn*"
 ssh-keygen -b 4092 -f "$hn""_rsa" -N '' >> /dev/null
 cd ~
-
