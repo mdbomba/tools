05b7c06ed45048073f4c7bd8090aca5c41ee5f26
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2024-05-10 07:26:51 -0700

    Update inspec-node1.sh

--- inspec-node1.sh
+++ inspec-node1.sh
@@ -1,13 +1,3 @@
-# clear
-# echo 'Running stig scan of Ubuntu 20.04 and dumping results to cli and to output.htm file'
-# echo ''
-# inspec exec stig-canonicalubuntu20.04lts-cationly-1.2.0-1.tar.gz -t ssh://chef@node1 --password 'devsecops' --reporter cli html2:./output.htm
-# echo ''
-# echo 'Displaying result as htm file. Press any key to continue. '
-# echo ''
-# read -p ''
-# firefox ./output.htm
-
 PROFILE='stig-canonicalubuntu20.04lts-cationly-1.2.0-1.tar.gz'
 TARGET='node1'
 USER='chef'
