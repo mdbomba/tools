df7dd949729a7c41a1facac99b50d1ec41bc24c6
Mike Bomba <32136488+mdbomba@users.noreply.github.com>
2024-05-23 11:17:34 -0700

    20240523 Download Chef Packages

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Version 20240522
+# Version 20240523
 # Author Mike Bomba
 # 
 # Script downloads chef install files for ubuntu and redhat based distros only
@@ -21,42 +21,19 @@ while ! [[ "$project" =~ ^(chef|chef-workstation|chef-server|automate|inspec|sup
 channel=''
 while ! [[ "$channel" =~ ^(current|stable)$ ]]; do read -p "Enter chef product channel (current, stable): " channel; done
 
+echo "Current machine architecture is `uname -m`"
+machine=''
+while ! [[ "$machine" =~ ^(x86_64|aarch64|i386|sparc)$ ]]; do read -p "Enter target machine architecture (x86_64, aarch64, i386 ): " machine; done
+
 ############
 # End of data input section
 ############
-
-# Assign a download directory
-tmp_dir="." 
-
-# If tmp_dir does not exist, create it
-if ! test -d $tmp_dir; then mkdir $tmp_dir; chmod 777 $tmp_dir; fi
-
-
-# normalize the architecture we detected
-machine=`uname -m`
-case $machine in
-  "arm64"|"aarch64")
-    machine="aarch64"
-    ;;
-  "x86_64"|"amd64"|"x64")
-    machine="x86_64"
-    ;;
-  "i386"|"i86pc"|"x86"|"i686")
-    machine="i386"
-    ;;
-  "sparc"|"sun4u"|"sun4v")
-    machine="sparc"
-    ;;
-esac
-
-os=`uname -s`
-
 
 ############
 # Chef Automate Download
 ############
 if [ "$project" = "automate" ]; then
-  download_filename="$tmp_dir/chef-automate"
+  download_filename="./chef-automate"
   download_url="https://packages.chef.io/files/current/latest/chef-automate-cli/chef-automate_linux_amd64.zip"
   curl -A "User-Agent: mixlib-install/3.12.30" --retry 5 -sL "${download_url}" | gunzip - > "$download_filename" && chmod +x "$download_filename"
   wget --user-agent="User-Agent: mixlib-install/3.12.30" -O "automate_`date -I`.aib" https://packages.chef.io/airgap_bundle/current/automate/latest.aib
@@ -75,7 +52,7 @@ fi
 ############
 # Fetch metadata (url, sha256hash)
 ############
-metadata_filename="$tmp_dir/metadata.txt"
+metadata_filename="./metadata.txt"
 metadata_url="https://omnitruck.chef.io/$channel/$project/metadata?v=$version&p=$platform&pv=$platform_version&m=$machine"
 wget --user-agent="User-Agent: mixlib-install/3.12.30" -O "$metadata_filename" "$metadata_url"
 echo ''
@@ -106,7 +83,7 @@ if [ "x${sha256}" = "x" ]; then echo "downloaded metadata file is corrupted or a
 ############
 filename=`echo $download_url | sed -e 's/?.*//' | sed -e 's/^.*\///'`
 filetype=`echo $filename | sed -e 's/^.*\.//'`
-download_filename="$tmp_dir/$filename"
+download_filename="./$filename"
 wget --user-agent="User-Agent: mixlib-install/3.12.30" -O "$download_filename" "$download_url" 
 ############
 # end fetch package
